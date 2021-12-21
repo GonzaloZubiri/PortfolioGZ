9adfdce5314b2babcc38d8921417cb1fcb155827
GonzaloZubiri <gonzalozubiri24@gmail.com>
2021-12-20 22:40:14 -0300

    second commit

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+
+$destino = "gonzalozubiri24@gmail.com";
+$name = $_POST["name"];
+$email = $_POST["email"];
+$asunto = $_POST["asunto"];
+$message = $_POST["message"];
+$content = "Name: " . $name . "\nEmail: " . $email . "\nAsunto: " . $asunto . "\nMessage: " . $message; 
+mail($destino,"Contacto", $content);
+header("Location:gracias.html");
+
+
+$email->send();
+        $alert = '<div class="alert-success">
+                    <span>Mensaje enviado! Gracias por contactarse.</span>
+                 </div>';
+    } catch (Exception $e){
+        $alert = '<div class="alert-success">
+                    <span>Algo salio mal! Por favor intente nuevamente.</span>
+                 </div>';
+    }
+
+
+?>
